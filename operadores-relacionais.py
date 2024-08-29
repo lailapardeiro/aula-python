ef22b660112fa9bacba6e7843ab72a8753e78020
lailapardeiro <laila.pardeiro@outlook.com>
2024-08-29 19:39:17 -0300

    Realização de Exercícios!

--- operadores-relacionais.py
+++ operadores-relacionais.py
@@ -27,3 +27,35 @@ print(resultado)
 resultado = a <= b 
 print(resultado)
 
+# Exercício 
+
+# Declarando variáveis
+
+dia = int(input('Qual o dia de hoje? '))
+pedidoPizza = int(input('Quantas pizzas comprou? '))
+pedidoBebida = int(input('Quantas bebidas comprou? '))
+pedidoBolo = int(input('Quantos bolos comprou? '))
+pedidoDoce = int(input('Quantos doces comprou? '))
+
+diaFesta = 26
+pedidoMinimoPizza = 10
+pedidoMinimoBebida = 12
+pedidoMinimoBolo = 5
+pedidoMinimoDoce = 600
+
+if diaFesta == (dia):
+    print('Ana está fazendo as compras no dia da festa!')
+else:
+    print('Ana está fazendo a compra adiantada')
+
+if(pedidoMinimoPizza) >= pedidoPizza:
+    print('Ana não comprou pizzas suficientes!')
+
+if(pedidoMinimoBebida) < pedidoBebida:
+    print('Ana comprou mais bebidas que o necessário!')
+
+if(pedidoMinimoBolo) <= pedidoBolo:
+    print('Ana excedeu a compra de bolos!')
+
+if(pedidoMinimoDoce) > pedidoDoce:
+    print('Ana não comprou doces suficientes!')
