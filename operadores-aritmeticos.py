e16df55178c25259b5003c7871e61ae084af3abc
lailpardeiro <laila.pardeiro@outlook.com>
2024-08-28 21:27:38 -0300

    arquivo adicionado

---
+++ operadores-aritmeticos.py
@@ -0,0 +1,31 @@
+a = 5
+b = 3 
+c = 4
+
+# Adição (+)
+resultado = a + b + c
+print(f'A soma é de: {resultado}')
+
+# Subtração (-)
+resultado = c - b
+print(f'A subtração é de: {resultado}')
+
+# Multiplicação (*)
+resultado = b * a 
+print(f'A multiplicação é de: {resultado}')
+
+# Divisão (/)
+resultado = a / b 
+print(f'A divisão é de: {resultado}')
+
+# Divisão Inteira (//)
+resultado = b // c 
+print(f'A divisão inteira é de: {resultado}')
+
+# Resto da Divisão (%)
+resultado = c % a 
+print(f'O resto da divisão é de: {resultado}')
+
+# Exponêncial (**)
+resultado = b ** a 
+print(f'O resultado da exponênciação é de: {resultado}')
