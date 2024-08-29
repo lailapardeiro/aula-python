6c9ab96f09022e84f61a93b9b1096d23ea99922b
lailpardeiro <laila.pardeiro@outlook.com>
2024-08-28 22:00:00 -0300

    Realização dos operadores relacionais

---
+++ operadores-relacionais.py
@@ -0,0 +1,27 @@
+a = 5 
+b = 3 
+
+# Igualdade
+resultado = a == b
+print(resultado)
+
+# Diferença
+resultado = a != b
+print(resultado)
+
+# Maior que (>)
+resultado = a > b
+print(resultado)
+
+# Menor que (<)
+resultado = a < b
+print(resultado)
+
+# Maior ou igual (>=)
+resultado = a >= b
+print(resultado)
+
+# Menor ou igual (<=)
+resultado = a <= b 
+print(resultado)
+
