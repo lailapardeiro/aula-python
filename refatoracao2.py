fa3d097f9949b2b1c4dfe03a1b84450612c61151
lailapardeiro <laila.pardeiro@outlook.com>
2024-09-04 21:46:07 -0300

    Realização de Refatoração

--- refatoracao2.py
+++ refatoracao2.py
@@ -1,17 +1,30 @@
-# O usuário irá colocar a idade desejada, o int ajuda a receber apenas números inteiros
-idade = int(input('Digite a idade:'))
-classificacao =  ' ' 
-# Se a idade colocada for menor que 13 anos, é considearado criança
-if idade < 13:
-    classificacao = "criança"
-# Se a idade colocada for igual ou maior que 13 e menor que 18 é considerado um adolescente
-elif idade >= 13 and idade < 18: 
-    classificacao = "adolescente"
-# Se a idade colocada for igual ou maior que 18 e menor que 60 é considerado um adulto
-elif idade >= 18 and idade < 60:
-    classificacao = "adulto"
-# Se a idade colocada for igual ou maior que 60 anos é considerado um idoso
-else: 
-    classificacao = 'idoso'
+# Variáveis Globais
+idade = int(input('Digite a idade desejada: '))
 
-print(f'A pessoa é classificada como {classificacao}')
+# Será validado a idade da persona para analisar sua categoria
+def validar_idade(idade):
+    mensagem_padrao = 'A pessoa é classificada como: '
+    if idade < 0:
+        return 'Essa por a caso já nasceu?'
+    
+    elif idade > 0 and idade <= 2:
+        return mensagem_padrao + 'bebê!' 
+    
+    elif idade > 2 and idade < 13:
+        return mensagem_padrao + 'criança!'
+
+    elif idade >= 13 and idade < 18: 
+        return mensagem_padrao + 'adolescente!'
+
+    elif idade >= 18 and idade <= 60:
+        return mensagem_padrao + 'adulto!'
+
+    elif idade > 60 and idade <= 105:
+        return mensagem_padrao + 'idoso!'
+    
+    else:
+        return 'Alguém verificia se está vivo?!'
+
+categoria_idade = validar_idade(idade)
+
+print(categoria_idade)
