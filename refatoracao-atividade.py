d2d41250087048bd4ba3b04b8a6c1ff0fcf483c7
lailapardeiro <laila.pardeiro@outlook.com>
2024-09-04 21:19:21 -0300

    Realização de Refatoração

--- refatoracao-atividade.py
+++ refatoracao-atividade.py
@@ -1,13 +1,17 @@
-idade = 25 
-
-categoria = 'indefinida'
+# O usuário irá colocar a idade desejada, o int ajuda a receber apenas números inteiros
+idade = int(input('Digite a idade:'))
+classificacao =  ' ' 
+# Se a idade colocada for menor que 13 anos, é considearado criança
 if idade < 13:
-    categoria = 'criança'
+    classificacao = "criança"
+# Se a idade colocada for igual ou maior que 13 e menor que 18 é considerado um adolescente
 elif idade >= 13 and idade < 18: 
-    categoria = 'adolescente'
-elif idade > 18 and idade < 60:
-    categoria = 'adulto'
+    classificacao = "adolescente"
+# Se a idade colocada for igual ou maior que 18 e menor que 60 é considerado um adulto
+elif idade >= 18 and idade < 60:
+    classificacao = "adulto"
+# Se a idade colocada for igual ou maior que 60 anos é considerado um idoso
 else: 
-    categoria = 'idoso'
+    classificacao = 'idoso'
 
-print('A pessoa é classificada como> ', categoria)
+print(f'A pessoa é classificada como {classificacao}')
