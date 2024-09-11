56f931d8bedffa20f1fc9c0cff1fc8adfba0bfbe
lailapardeiro <laila.pardeiro@outlook.com>
2024-09-11 19:58:13 -0300

    Realização Desafio 1

--- desafios/desafio1.py
+++ desafios/desafio1.py
@@ -1,1 +1,10 @@
+# Desafio 1: Concatenar
+    # Concatenar Strings
+        #  ● Crie um programa que solicite ao usuário o seu nome e sobrenome e, em seguida, exiba uma mensagem de boas vindas completa no formato: "Bem-vindo, [Nome] [Sobrenome]!".
+
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+print(f'Bem vindo {nome} {sobrenome}!')
+
+
 
